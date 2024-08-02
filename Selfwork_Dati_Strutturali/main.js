b098ecd9bf17626a98eb3b9d2b74359c04e2376e
LisviaIlvia <silviaercolino@yahoo.it>
2024-08-02 10:19:28 +0200

    Add files via upload

---
+++ Selfwork_Dati_Strutturali/main.js
@@ -0,0 +1,72 @@
+
+// Stampa a schermo la frase: "Un Anello per domarli, un Anello per trovarli, un Anello per ghermirli e nel buio incatenarli.“
+
+let array_1 = [
+    ['un', 'per', 'incatenarli.'],
+    ['Anello', 'trovarli,'],
+    ['ghermirli', 'e'],
+    ['gondor', 'mark'],
+];
+
+let array_2 = [
+    [['trovarli,']],
+    ['tu,', 'sciocchi'],
+    ['tu,', 'sciocchi', ['padron', 'Sauron']],
+    ['nel', ['fuggite', 'gandalf']],
+    [['domarli,', 'passare'], 'buio']
+];
+
+// Parole necessarie (nell'ordine):
+// "Un"
+// "Anello"
+// "per"
+// "domarli,"
+// "un"
+// "Anello"
+// "per"
+// "trovarli,"
+// "un"
+// "Anello"
+// "per"
+// "ghermirli"
+// "e"
+// "nel"
+// "buio"
+// "incatenarli."
+
+let parole = [
+    array_1[0][0],
+    array_1[1][0],
+    array_1[0][1],
+    array_2[4][0][0],
+    array_1[0][0],
+    array_1[1][0],
+    array_1[0][1],
+    array_1[1][1],
+    array_1[0][0],
+    array_1[1][0],
+    array_1[0][1],
+    array_1[2][0],
+    array_1[2][1],
+    array_2[3][0],
+    array_2[4][1],
+    array_1[0][2]
+];
+
+let frase = '';
+
+for (let i = 0; i < parole.length; i++) {
+    if (i > 0) {
+        frase += ' ';
+    }
+    frase += parole[i];
+}
+
+console.log(frase);
+
+
+
+
+
+
+
